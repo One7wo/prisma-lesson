da5b81d002714ce691deb15bfb6e063558ccebba
OneTwo <gonchar.dev@yandex.ru>
2024-03-12 23:32:08 +0300

    fix - problem fix default test

--- src/articles/articles.controller.spec.ts
+++ src/articles/articles.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ArticlesController } from './articles.controller';
 import { ArticlesService } from './articles.service';
+import {PrismaService} from "../prisma/prisma.service";
 
 describe('ArticlesController', () => {
   let controller: ArticlesController;
@@ -8,10 +9,10 @@ describe('ArticlesController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ArticlesController],
-      providers: [ArticlesService],
+      providers: [ArticlesService, PrismaService],
     }).compile();
 
-    controller = module.get<ArticlesController>(ArticlesController);
+    controller = module.get<ArticlesController, ArticlesController>(ArticlesController);
   });
 
   it('should be defined', () => {
--- src/articles/articles.service.spec.ts
+++ src/articles/articles.service.spec.ts
@@ -1,15 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ArticlesService } from './articles.service';
+import {PrismaService} from "../prisma/prisma.service";
 
 describe('ArticlesService', () => {
   let service: ArticlesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ArticlesService],
+      providers: [ArticlesService, PrismaService],
     }).compile();
 
-    service = module.get<ArticlesService>(ArticlesService);
+    service = module.get<ArticlesService, ArticlesService>(ArticlesService);
   });
 
   it('should be defined', () => {
